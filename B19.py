8f4bfd1338d09829a9863c875d3c5245b2ccc275
Thùy Ngân <nguyenthithuyngan2807@Gmail.com>
2024-09-03 18:36:07 +0700

    B19.py

--- B19.py
+++ B19.py
@@ -2,11 +2,12 @@ a=int(input("Số nguyên a: "))
 b=int(input("Số nguyên b: "))
 c=int(input("Số nguyên c: "))
 d=int(input("Số nguyên d: "))
-if (a<b and a<c and a<d) or (a==b and a<c and a<d) or (a==c and a<b and a<d) or (a==d and a<b and a<c):
-    print("Số có giá trị nhỏ nhất là:",a)
-elif (b<a and b<c and b<d) or (a==b and b<c and b<d) or (b==c and b<a and b<d) or (b==d and b<a and b<c):
+#Giả sử a là số có giá trị nhỏ nhất.
+if b<a:
     print("Số có giá trị nhỏ nhất là:",b)
-elif (c<b and c<a and c<d) or (c==b and c<a and c<d) or (a==c and c<b and c<d) or (c==d and c<b and c<a):
+elif c<a:
     print("Số có giá trị nhỏ nhất là:",c)
-else:
+elif d<a:
     print("Số có giá trị nhỏ nhất là:",d)
+else:
+    print("Số có giá trị nhỏ nhất là:",a)
